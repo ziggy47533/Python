739f0f40963f6b5ce247b42b3e395d3d6e7bc536
ziggy47533 <41532539+ziggy47533@users.noreply.github.com>
2022-03-13 06:57:38 -0700

    Add two Py
    Hello Default

---
+++ Hello.py
@@ -0,0 +1,11 @@
+# Python using Function
+
+# Create a function called Hello
+def Hello_Func () :
+    print ("Hello World!")
+
+# Print function do nothing
+print (Hello_Func)
+
+# Calling the Function
+Hello_Func ()
