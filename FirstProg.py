1c2cbdb5689a3fd09801c430ca99ef80794761bc
ziggy47533 <41532539+ziggy47533@users.noreply.github.com>
2022-03-13 06:56:43 -0700

    Add two Py
    First ever Py code

---
+++ FirstProg.py
@@ -0,0 +1,46 @@
+# First Python Demo
+
+print("Hello World!!!")
+
+print("This is python")
+
+print("How to program in python")
+
+print("This is how you use the 'print' function in VS Code")
+
+print("Hi, My name is Maggie")
+
+# User input
+Name = (input("What is your name? ")).title()
+
+print("Hello " + Name + "," + " " + "glad to meet you! ")
+
+User = input("Please enter your name ").title()
+print("Hello, " + User)
+
+
+# Display Test
+print("This is how you dispaly a text")
+print("How to display text to the console")
+
+print("This great use of the print statement")
+
+a = 1
+
+if a > 4 and a < 10:
+    a += 1
+    print("Granted")
+else:
+    print("Denied")
+
+
+
+# ues the  module random and it import for random
+
+import random
+from random import randint
+from math import *
+
+print(randint( 5, 10 ) )
+print(factorial (4 ) )
+print(random.random( ) )
